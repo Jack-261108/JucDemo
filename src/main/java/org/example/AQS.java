5a9a5cb18963140f9ffd3b5cba4f9f96994ecc86
Jack <2091813161@qq.com>
2023-11-24 22:37:29 +0800

    Initial commit

---
+++ src/main/java/org/example/AQS.java
@@ -0,0 +1,9 @@
+package org.example;
+
+/**
+ * @author Jack
+ * @Desc
+ * @Date 2023/11/24/16:18:25
+ **/
+public class AQS {
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @author Jack
+ * @Desc
+ * @Date 2023/11/24/16:17:16
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
